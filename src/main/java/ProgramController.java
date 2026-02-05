5aa32ef7e0783937974484608fb18be222ccfcde
Carlos Giron <carlosegiron92@gmail.com>
2026-02-05 13:02:02 -0500

    commiting test.java

---
+++ src/main/java/ProgramController.java
@@ -0,0 +1,14 @@
+public class ProgramController {
+    public void execute(UserRequest request){
+
+    }
+    public void handleList(){
+
+    }
+    private void handleShow(){
+
+    }
+    private void printError(){
+
+    }
+}
