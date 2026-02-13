47f45d18d0bf8defd14291e1b8845f20a8d07267
Carlos Giron <carlosegiron92@gmail.com>
2026-02-13 16:13:30 -0500

    dummy implemntation of CommandLineParser class and mostly finished TopSecret class

--- src/main/java/ProgramController.java
+++ src/main/java/ProgramController.java
@@ -36,8 +36,16 @@ public class ProgramController {
         return ret;
     }
     public String handleShow(int idx){
-        String output = files.get(idx-1);
-        return fileHandler.readFile(fileHandler.getDataDir()+"/"+output);
+        try {
+            String output = files.get(idx-1);
+            return fileHandler.readFile(fileHandler.getDataDir()+"/"+output);
+        }
+        catch(IndexOutOfBoundsException e){
+            System.out.println("Error: Index " + idx + " is out of bounds. Current list size is " + files.size() + ".");
+            System.out.println("Try again please.");
+            System.exit(0);
+        }
+        return "";
     }
     private void printError(){
         System.out.println("Error ig. idk lol.");
--- src/main/java/TopSecret.java
+++ src/main/java/TopSecret.java
@@ -1,8 +1,11 @@
+import javax.crypto.Cipher;
+import java.io.IOException;
+
 /**
  * Commmand Line Utility
  */
 public class TopSecret {
-    public static void main(String[] args) {
+    public static void main(String[] args) throws IOException {
         CommandLineParser parser = new CommandLineParser(args);
         String selection = parser.placeholder;
 
@@ -10,7 +13,14 @@ public class TopSecret {
         fileHandler.terminalDataDir();
         ProgramController programController = new ProgramController(fileHandler);
         UserRequest userRequest = new UserRequest(selection);
+        String text = programController.execute(userRequest);
 
-        System.out.println(programController.execute(userRequest));
+        if (args.length == 3) {
+            KeyLoader keyLoader = new KeyLoader();
+            CipherKey cipherKey = keyLoader.loadKey(args[3]);
+            CipherService cipherService = new CipherService();
+            text = cipherService.decipher(text, cipherKey);
+        }
+        System.out.println(text);
     }
 }
